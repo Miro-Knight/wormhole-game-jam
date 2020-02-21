90db03c8c28ff4293a9ad6a0b19904f9c5cda746
Miro-Knight <61126259+Miro-Knight@users.noreply.github.com>
2020-02-20 21:20:52 -0600

    Changed Object Pool Numbers and Fixed Scripts

--- Assets/Scripts/Cactus.cs
+++ Assets/Scripts/Cactus.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoleSpawner : MonoBehaviour
+public class Cactus : MonoBehaviour
 {
     public float spawnEvery;
 
@@ -15,6 +15,6 @@ public class HoleSpawner : MonoBehaviour
     }
     void SpawnWithDelay()
     {
-        objectPool.SpawnFromPool("SafetyCone");
+        objectPool.SpawnFromPool("Cactus");
     }
 }
